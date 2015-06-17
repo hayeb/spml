e099c03fdc93988d256a54a5641a187665ee1752
hayeb <haye.bohm@gmail.com>
2015-06-17 09:46:33 +0200

    Added ValueItration Learner skeleton.

---
+++ spml4/src/nl/ru/ai/HayeBohmWesleyvanHoorn/reinforcement/ReinforcementLearning.java
@@ -0,0 +1,14 @@
+package nl.ru.ai.HayeBohmWesleyvanHoorn.reinforcement;
+
+public class ReinforcementLearning {
+
+	public ReinforcementLearning() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ spml4/src/nl/ru/ai/vroon/mdp/ValueIIterationLearner.java
@@ -0,0 +1,14 @@
+package nl.ru.ai.vroon.mdp;
+
+public class ValueIIterationLearner {
+	private MarkovDecisionProblem mdp ;
+
+	public ValueIIterationLearner(MarkovDecisionProblem mdp) {
+		this.mdp = mdp;
+	}
+	
+	public void learn() {
+		
+	}
+
+}

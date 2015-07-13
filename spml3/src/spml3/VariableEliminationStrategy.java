75f4fb1dd2d836901625d9c7f6cbb8797c2cb049
Casper <Casper@laptop_Casper>
2015-07-13 17:37:37 +0200

    Very rough variable elimination explained and partly implemented

--- spml3/src/spml3/VariableEliminationStrategy.java
+++ spml3/src/spml3/VariableEliminationStrategy.java
@@ -4,11 +4,53 @@ import java.util.ArrayList;
 
 public class VariableEliminationStrategy implements ProbabilityCalculationStrategy {
 	private ArrayList<BeliefNode> eliminationOrdering;
+	private ArrayList<Factor> factorlist;
+	
 	
 	public VariableEliminationStrategy(BeliefNetwork beliefnetwork){
 		eliminationOrdering = generateEliminationOrdering(beliefnetwork);
 	}
+
+	@Override
+	public double calculateProbability(String nodeName, String parents) {
+		if(!isInEliminationOrdering(nodeName))
+			for(BeliefNode beliefnode : eliminationOrdering){
+				Factor[] factorlist
+				Factor newfactor = multiplyFactors(beliefnode);
+				//sum out beliefnode variable from newfactor.
+				//remove factors in factorlist from main factorlist.
+				//add newfactor
+			}
+		return 0;
+	}
 	
+	/**
+	 * The current node/variable, before getting eliminated, multiply all factors including this variable,
+	 * creating one final variable.
+	 * @param beliefnode
+	 */
+	private Factor multiplyFactors(factorlist){
+		
+	}
+	
+	/**
+	 * Checks the EO for possible error.
+	 * @param nodeName
+	 * @return
+	 */
+	private boolean isInEliminationOrdering(String nodeName){
+		for(BeliefNode beliefnode : eliminationOrdering){
+			if(beliefnode.getName().equals(nodeName))
+				return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param beliefnetwork
+	 * @return
+	 */
 	private ArrayList<BeliefNode> generateEliminationOrdering(BeliefNetwork beliefnetwork) {
 		ArrayList<BeliefNode> eliminationordering = new ArrayList<BeliefNode>(beliefnetwork.getNodes().size()-1);
 		String[] str = new String[8];
@@ -24,26 +66,6 @@ public class VariableEliminationStrategy implements ProbabilityCalculationStrate
 		}
 		return eliminationordering;
 		
-	}
-
-	@Override
-	public double calculateProbability(String nodeName, String parents) {
-		if(!isInEliminationOrdering(nodeName))
-			return 1;
-		return 0;
-	}
-	
-	/**
-	 * Checks the EO for possible error.
-	 * @param nodeName
-	 * @return
-	 */
-	public boolean isInEliminationOrdering(String nodeName){
-		for(BeliefNode beliefnode : eliminationOrdering){
-			if(beliefnode.getName().equals(nodeName))
-				return true;
-		}
-		return false;
 	}
 
 }

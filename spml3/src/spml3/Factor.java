a5287067c8a4a8fc3ce65d1792bfe99326fda924
mmwvh <wvhoorn92@gmail..com>
2015-07-15 15:45:39 +0200

    Ik heb geen idee

--- spml3/src/spml3/Factor.java
+++ spml3/src/spml3/Factor.java
@@ -10,16 +10,33 @@ package spml3;
 public class Factor {
 
 	/**
-	 * lijst met nodes
+	 * heeft een lijst met parents per node : factor
 	 */
+	public Factor() {
+		
+	}
+
+	/**
+	 * Geeft een lijst met factoren waar de to be eliminated variable voorkomt en verwijderd deze uit de factor lijst
+	 * @return lijst met probability tabellen die een parent bevat die gelijk is aan to be eliminated variable
+	 */
+	public lijst tbMultiplied() {
+		for alle nodes
+			for alle parents
+				gelijk aan to be eliminated variable?
+						voeg toe aan lijst
+						verwijder uit factor lijst
+						
+		return lijst
+		
+	}
 	
 	
 	/**
-	 * lijst met factor verwijderen
+	 * 
 	 */
-	
-	/*
-	 * nieuwe factor aan lijst toevoegen
-	 */
+	public void addFactor() {
+		
+	}
 	
 }

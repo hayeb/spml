d5280c2fddeeaa86684e2b60e65a55d6498d5e37
Casper <Casper@laptop_Casper>
2015-07-16 14:26:21 +0200

    constructor in Factor class to create Factor from BeliefNode

--- spml3/src/spml3/Factor.java
+++ spml3/src/spml3/Factor.java
@@ -3,21 +3,30 @@
  */
 package spml3;
 
+import java.util.Map;
+
 /**
  * @author mmwvh
  *
  */
 public class Factor {
+	private String[] variableNames;
+	private Map<boolean[], Double> probabilities;
 
 	/**
 	 * heeft een lijst met parents per node : factor
 	 */
-	public Factor() {
+	public Factor(BeliefNode beliefnode) {
+		variableNames = new String[beliefnode.getParentNames().length+1];
+		variableNames[0] = beliefnode.getName();
+		for(int i = 0; i < beliefnode.getParentNames().length; i++){
+			variableNames[i+1] = beliefnode.getParentNames()[i];
+		}
 		
 	}
 
 	/**
-	 * Geeft een lijst met factoren waar de to be eliminated variable voorkomt en verwijderd deze uit de factor lijst
+	 * Geeft een lijst met factoren waar de to be eliminated variable voorkomt en verwijdert deze uit de factor lijst
 	 * @return lijst met probability tabellen die een parent bevat die gelijk is aan to be eliminated variable
 	 */
 	public lijst tbMultiplied() {

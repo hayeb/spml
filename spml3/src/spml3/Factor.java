7b430933f8ad8821833cf50f5e2347e98b050653
Haye Bohm <haye.bohm@gmail.com>
2015-07-22 11:59:10 +0000

    Clean-up before merging with master branch

--- spml3/src/spml3/Factor.java
+++ spml3/src/spml3/Factor.java
@@ -33,20 +33,20 @@ public class Factor {
 		}
 	}
 
-	/**
-	 * Geeft een lijst met factoren waar de to be eliminated variable voorkomt en verwijdert deze uit de factor lijst
-	 * @return lijst met probability tabellen die een parent bevat die gelijk is aan to be eliminated variable
-	 */
-	public lijst tbMultiplied() {
-		for alle nodes
-			for alle parents
-				gelijk aan to be eliminated variable?
-						voeg toe aan lijst
-						verwijder uit factor lijst
-						
-		return lijst
-		
-	}
+//	/**
+//	 * Geeft een lijst met factoren waar de to be eliminated variable voorkomt en verwijdert deze uit de factor lijst
+//	 * @return lijst met probability tabellen die een parent bevat die gelijk is aan to be eliminated variable
+//	 */
+//	public lijst tbMultiplied() {
+//		for alle nodes
+//			for alle parents
+//				gelijk aan to be eliminated variable?
+//						voeg toe aan lijst
+//						verwijder uit factor lijst
+//						
+//		return lijst
+//		
+//	}
 	
 	
 	/**

fe05a7af0f2fb84aa8b36108abe20f06cf7e592a
Haye Bohm <haye.bohm@gmail.com>
2015-07-22 12:11:13 +0000

    Formatting changes.

--- spml3/src/spml3/BifParser.java
+++ spml3/src/spml3/BifParser.java
@@ -5,8 +5,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Locale;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
